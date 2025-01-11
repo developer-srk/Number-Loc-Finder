ec292d98a01b734f1c50c0c15c28666bca728ebb
Developer SRK <sairajkhanvilkar7@gmail.com>
2025-01-11 15:59:05 +0530

    Update phoneLoc.py

--- phoneLoc.py
+++ phoneLoc.py
@@ -1,6 +1,6 @@
 import phonenumbers
 from phonenumbers import geocoder
-phone_number1 = phonenumbers.parse("+917620014422")
+phone_number1 = phonenumbers.parse("+911121222")
 
 print("/nPhone Number Location/n")
 print(geocoder.description_for_number(phone_number1,"en"))
